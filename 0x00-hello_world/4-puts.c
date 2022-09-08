2b3351e92a6ff6368571c998defde79db984fbe7
Francis Tarus <francis.tarus.ft@gmail.com>
2022-09-08 16:13:25 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - print the string in the put function
+*
+*Description: using the main fuction
+*this program prints"programming is like building a multilingual puzzle
+*/
+int main(void)
+{
+      puts("\"programming is like building a multilingualpuzzle");[5~
+      return(0);
+}
