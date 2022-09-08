33b95972c64ef1e783e4e03b7db7bb20c3dd414e
Francis Tarus <francis.tarus.ft@gmail.com>
2022-09-08 16:26:00 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print the string in the put function
+*
+*Description : using the main functin 
+* this program prints "programming is building a multilingual puzzle
+*return: 0
+*/
+int main(void)
+{
+   printf("with proper grammer, but the outcome is a piece of art, \n");
+   return (0);
+}
